cbc5db66b684bfb9b6b4daa8541bf96311ce87a9
rsancho64 <rsancho@iesrioarba.es>
2024-10-09 12:41:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# un estudio de TAD's implementando el TAD pila
+
+
+usamos como tipo base el tipo list de python
+que aporta mucho trabajo hecho.
+(esto será un wrapper y poco mas)
+
+## diseño:
+
+    datos: la pila es generica. (apila cualquier dato,, tipo: object)
+    operaciones:
+
+- [x] push()
+- [x] pop()
+- [x] h()    // altura 
+- [x] drop() // descarte 
+- [x] esVacia()
+- [x] esLlena()
+
---
+++ pila.py
@@ -0,0 +1,85 @@
+#! /usr/bin/python3
+
+class mipila:
+
+    # __h = 0
+    __data = []
+    __hmax = None
+
+    def __init__(self, hmax=10):
+        # self.__data = []
+        self.__hmax = hmax
+
+    def __str__(self) -> str:
+        answ = "<"
+        answ += f"{self.h()} de {self.__hmax}, {self.__data}"
+        answ += ">"
+        return answ
+
+    def push(self,algo):
+        if not self.esLlena():
+            self.__data.append(algo)
+
+    def pop(self):
+        try:
+            return self.__data.pop()
+        except IndexError:
+            return None
+
+    def esLlena(self) -> bool:
+        if len(self.__data) == self.__hmax:
+            return True
+        return False
+
+    def esVacia(self) -> bool:
+        return not self.esLlena()
+
+    def h(self) -> int:
+        return len(self.__data)
+
+    def drop(self):
+        self.__data = []
+
+if __name__ == "__main__":
+
+    # mp = mipila()
+    # print(mp)
+
+    mp3 = mipila(3)
+    print(mp3, mp3.esLlena())
+
+    mp3.push(11)
+    print(mp3, mp3.esLlena())
+
+    mp3.push(33)
+    print(mp3, mp3.esLlena())
+
+    mp3.push(22)
+    print(mp3, mp3.esLlena())
+
+    mp3.push("doblada")
+    print(mp3, mp3.esLlena())
+
+    # mp3.drop()
+    # print(mp3, mp3.esLlena())
+
+    item = mp3.pop()
+    print(mp3, mp3.esLlena(), "item: ", item)
+
+    item = mp3.pop()
+    print(mp3, mp3.esLlena(), "item: ", item)
+
+    item = mp3.pop()
+    print(mp3, mp3.esLlena(), "item: ", item)
+
+    item = mp3.pop()
+    print(mp3, mp3.esLlena(), "item: ", item)
+
+    mp3.push(None)
+    print(mp3, mp3.esLlena())
+
+    mp3.push("algo mas")
+    print(mp3, mp3.esLlena())
+
+    mp3.push(None)
+    print(mp3, mp3.esLlena())
