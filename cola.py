6f5eb9298a9f68eab04758459f8ec3338e4bd10b
rsancho64 <rsancho@iesrioarba.es>
2024-10-10 12:12:06 +0200

    .

--- cola.py
+++ cola.py
@@ -4,44 +4,44 @@ class micola:
     """
     TAD cola sobre la class lista"""
 
-    __data = []
-    __hmax = None # todo refact: lmax
+    # __data = []
+    # __hmax = None # todo refact: lmax
 
-    def __init__(self, hmax=10):
-        # self.__data = []
-        self.__hmax = hmax
+    # def __init__(self, hmax=10):
+    #     # self.__data = []
+    #     self.__hmax = hmax
 
-    def __str__(self) -> str:
-        answ = "<"
-        answ += f"{self.h()} de {self.__hmax}, {self.__data}"
-        if self.esLlena():
-            answ += " LLENA"
-        answ += ">"
-        return answ
+    # def __str__(self) -> str:
+    #     answ = "<"
+    #     answ += f"{self.h()} de {self.__hmax}, {self.__data}"
+    #     if self.esLlena():
+    #         answ += " LLENA"
+    #     answ += ">"
+    #     return answ
 
-    def push(self, algo):
-        if not self.esLlena():
-            self.__data.append(algo)
+    # def push(self, algo):
+    #     if not self.esLlena():
+    #         self.__data.append(algo)
 
-    def pop(self):
-        try:
-            return self.__data.pop()
-        except IndexError:
-            return None
+    # def pop(self):
+    #     try:
+    #         return self.__data.pop()
+    #     except IndexError:
+    #         return None
 
-    def esLlena(self) -> bool:
-        if len(self.__data) == self.__hmax:
-            return True
-        return False
+    # def esLlena(self) -> bool:
+    #     if len(self.__data) == self.__hmax:
+    #         return True
+    #     return False
 
-    def esVacia(self) -> bool:
-        return not self.esLlena()
+    # def esVacia(self) -> bool:
+    #     return not self.esLlena()
 
-    def h(self) -> int:
-        return len(self.__data)
+    # def h(self) -> int:
+    #     return len(self.__data)
 
-    def drop(self):
-        self.__data = []
+    # def drop(self):
+    #     self.__data = []
 
 
 if __name__ == "__main__":
